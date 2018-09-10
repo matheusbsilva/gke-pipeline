580a6550c7ab9240f0cb6156872e94a1bd9d7afc
matheusbsilva <matheusbattista@hotmail.com>
2018-09-10 13:43:50 -0300

    Add serverjs file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.json({"message": "hello world"});
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
