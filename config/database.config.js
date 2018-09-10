dbad8295ee674840f5167e26748f5123d184951d
matheusbsilva <matheusbattista@hotmail.com>
2018-09-10 19:47:51 -0300

    Add k8s manifests

--- config/database.config.js
+++ config/database.config.js
@@ -1,7 +1,5 @@
-const HOST = process.env.MONGO_HOST || "db";
-const PORT = process.env.MONGO_PORT || 27017;
-const DB_NAME = process.env.DB_NAME || "dashboard-db"; 
+const url = "mongodb://mongo:27017/dashboarddb"
 
 module.exports = {
-  url: `mongodb://${HOST}:${PORT}/${DB_NAME}`
+  url: url 
 }
